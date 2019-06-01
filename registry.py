7818a75f15e1bbdd34dd9fda0ee5cd613d9a523a
kirillkuzin <offkirillkuzin@gmail.com>
2019-06-01 16:59:39 +0300

    Update

--- registry.py
+++ registry.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from ethereum_core import Ethereum
 
 class Registry(Ethereum):
@@ -26,9 +28,15 @@ class Registry(Ethereum):
     def __get_certificate(self, certificate_id):
         certificate_data = \
          self.registry_contract.call().get_certificate(certificate_id)
-        time_of_start_of_production_period = certificate_data[4]
-        time_of_stop_of_production_period = certificate_data[5]
-        time_of_destruction = certificate_data[6]
+        time_of_start_of_production_period = self.__from_timestamp(
+            certificate_data[4]
+        )
+        time_of_stop_of_production_period = self.__from_timestamp()
+            certificate_data[5]
+        )
+        time_of_destruction = self.__from_timestamp(
+            certificate_data[6]
+        )
         redeemed = "No"
         if certificate_data[7]:
             redeemed = "Yes"
@@ -49,3 +57,9 @@ class Registry(Ethereum):
 
     def __get_last_certificate_id(self):
         return self.registry_contract.call().last_certificate_id()
+
+    def __from_timestamp(self, timestamp):
+        datetime = datetime.utcfromtimestamp(
+            int(timestamp)
+        ).strftime('%Y-%m-%d %H:%M:%S')
+        return datetime
