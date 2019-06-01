4e5f03eb4795fb30c0ff9cb5c6b627a48b476326
kirillkuzin <offkirillkuzin@gmail.com>
2019-06-01 21:52:43 +0300

    Update

--- settings_local.py
+++ settings_local.py
@@ -9,7 +9,3 @@ ETHEREUM_SETTINGS = {
     "REGISTRY_CONTRACT_ADDRESS": os.getenv("REGISTRY_CONTRACT_ADDRESS"),
     "REGISTRY_CONTRACT_ABI": "abi/registry_contract_abi.json"
 }
-
-# DATABASE_SETTINGS = {
-#     "DATABASE_URL": os.getenv("DATABASE_URL")
-# }
