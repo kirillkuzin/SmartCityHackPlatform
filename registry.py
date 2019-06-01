23197a0ad063e67bb1c4e1d5bb4de4660860551c
kirillkuzin <offkirillkuzin@gmail.com>
2019-06-01 16:15:33 +0300

    Update

--- registry.py
+++ registry.py
@@ -1,6 +1,17 @@
 from ethereum_core import Ethereum
 
 class Registry(Ethereum):
+    ENERGY_TYPES = [
+        "Energy of sun",
+        "Wind energy",
+        "Water energy"
+        "Geothermal energy",
+        "Low potential thermal energy",
+        "Biomass",
+        "Biogas",
+        "Gas"
+    ]
+
     def get_all_certificates(self):
         last_certificate_id = self.__get_last_certificate_id()
         certificates = []
@@ -22,9 +33,9 @@ class Registry(Ethereum):
         if certificate_data[7]:
             redeemed = "Yes"
         certificate = {
-            "id": certificate_data[0],
+            "certificate_id": certificate_data[0],
             "generating_object": certificate_data[1],
-            "energy_type": certificate_data[2],
+            "energy_type": self.ENERGY_TYPES[certificate_data[2]],
             "amount_of_energy": certificate_data[3],
             "time_of_start_of_production_period": \
                 time_of_start_of_production_period,
